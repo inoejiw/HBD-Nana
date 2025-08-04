9e967e80cfdc0c786a69039d06430f1eda9deb46
Ihsan Nursyahjiwa <ihsan.nursyahjiwa1@gmail.com>
2025-08-04 12:00:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var nama = prompt('Haiii :)');
+
+alert('Halooo Nana ^^');
+
+alert('Kamu tau nggk, aku tuh bikin sama kirim website ini cuman buat kamuu loh... hhe, dan for the first time juga aku bikin ini');
+alert('jadi lewat program yang aku buat ini ada yang sebenernya pengen aku sampein, maaf yah kalo emang kesannya aku repotin kamu dan banyak tanya juga, soalnya ini pertama kalinya buat aku dalam hal beginian mungkin yahh...');
+alert('Jujur pas liat sg kamu yang tadi pagi yang isinya itu hari ini ternyata kamu ulang tahun, aku yang memang belum pernah ngalamin dirayakan atau merayakan cukup bingung, anjayy si teteh lagii ultahhh ini siaku kudu kumaha yahh... ');
+alert('Dan karena aku orangnya penasaran dan bingung pada akhirnya aku tanya langsung aja deh biar jelas juga hhe...');
+alert('Dan yapss pada akhirnya aku tanyain beneran dongg wkwk');
+alert('Terus sedikitnya aku jadi paham ketika kamu bilang untuk bersikap ketika ada orang yang ulang tahun itu kita bisa ucapin, doain, atau lakuin apa yang kita bisaa...');
+alert('Dann...');
+alert('Akutuh ada langsung kepikiran buat ngucapin dengan cara yang aku bisaa');
+alert('Dan Yapss ini cara yang aku bisa lakuin buat ngucapin ulang tahun kamu yang ke 23 di tahun 2025 ini...');
+alert('Maaf yah mungkin sangat amat sederhana tapi aku harap apa yang ingin aku ucapin tersampaikan buat kamu...');
+alert('Buat Ulang tahun kamu yang ke 23 ini aku pengen bilangg...');
+alert('Happy birthday Nadia... for 23 y\'o, I hope you can achieve all the things that you wish before. Maybe my word not that important but I want you to relize that you have a big potential, you\'re a smart, strong, kind, and pretty girl I\'ve ever seen. And I think everyone will agree with that. So, be happy and lets make your life more colorfull... :)');
+alert('Sorry if I ever make you mad... ');
+alert('Sorry if I ever make you uncomfortable...');
+alert('All the things i\'ve ever do before is just for one reason... "I want make your life happy" That\'s it.');
+alert('But, if what i try to do make your life uncomfortable please say it okay... so i\'ll stop it.');
+alert('Dan mungkin itu semua yang aku mau ungkapin, sekarang aku minta maaf kalo ada yang salah yahh... dan sekali lagii Happy Birthday Nadia Awalliah ^-^');
+alert('Dan yah mungkin ini jadi hal yang cukup awkward yah dihidup kamu tp yahh begitulah aku juga nggk nyangka bisa begini. Thanks yah... dan kalo butuh sesuatu kaya temen cerita, keluh kesah atau apapun itu yang bisa kubantu tinggal infoin aja yahh.');
